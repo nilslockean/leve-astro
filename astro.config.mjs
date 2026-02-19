ae1dd97c90a5fd7c1350087035497f3e3e11221a
Nils Lockean <nils.lockean@texthelp.com>
2026-02-19 13:53:41 +0100

    Refactor Astro configuration for improved readability, add Prettier for code formatting, and update package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from "@astrojs/sitemap";
 // Load environment variables from local .env file into process.env
 dotenv.config();
 
-// These variables are needed before Astro is initialized and sets up the runtime 
+// These variables are needed before Astro is initialized and sets up the runtime
 // environment variables so we need to access them directly from process.env
 const { SANITY_DATASET = "production", SANITY_TOKEN } = process.env;
 
@@ -21,26 +21,33 @@ if (!SANITY_TOKEN) {
 console.log("Early env variables:", {
   SANITY_DATASET,
   SANITY_TOKEN,
-})
+});
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bagerileve.se",
-  integrations: [sanity({
-    projectId: "mz20cm4o",
-    dataset: SANITY_DATASET,
-    apiVersion: "2026-02-19",
-    useCdn: false,
-    token: SANITY_TOKEN,
-  }), mdx(), sitemap({
-    filter: (page) => !page.includes("/partials/")
-  })],
+  integrations: [
+    sanity({
+      projectId: "mz20cm4o",
+      dataset: SANITY_DATASET,
+      apiVersion: "2026-02-19",
+      useCdn: false,
+      token: SANITY_TOKEN,
+    }),
+    mdx(),
+    sitemap({
+      filter: (page) => !page.includes("/partials/"),
+    }),
+  ],
   adapter: netlify(),
   image: {
     domains: ["cdn.sanity.io"],
   },
   server: {
-    allowedHosts: ["leve-astro-staging.netlify.app", "devserver-main--leve-astro-staging.netlify.app"],
+    allowedHosts: [
+      "leve-astro-staging.netlify.app",
+      "devserver-main--leve-astro-staging.netlify.app",
+    ],
   },
   env: {
     schema: {
@@ -48,7 +55,7 @@ export default defineConfig({
         context: "server",
         access: "public",
         default: "default value",
-      })
-    }
+      }),
+    },
   },
 });
