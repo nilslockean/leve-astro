0f0323e887aa0c5d32a37fa83041ea4265fb032d
Nils Lockean <nilslockean@gmail.com>
2026-03-12 18:20:31 +0100

    Make ISanityClient.fetch generic

--- src/lib/types/ISanityClient.ts
+++ src/lib/types/ISanityClient.ts
@@ -2,7 +2,10 @@ import type { SanityDocumentStub } from "@sanity/client";
 import type { SanityDocument } from "@sanity/client/csm";
 
 export interface ISanityClient {
-  fetch: (query: string, params?: Record<string, unknown>) => Promise<unknown>;
+  fetch: <T extends unknown>(
+    query: string,
+    params?: Record<string, unknown>,
+  ) => Promise<T>;
   create?: <R extends Record<string, any> = Record<string, any>>(
     document: SanityDocumentStub<R>,
   ) => Promise<SanityDocument<R>>;
