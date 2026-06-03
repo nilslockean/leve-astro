1955b5b34f81cecfdeec58e02a151c7eaae1e83a
Nils Lockean <nils@lockean.se>
2026-06-03 09:07:58 +0200

    Add support for products without images

--- src/lib/schemas/Product.ts
+++ src/lib/schemas/Product.ts
@@ -10,14 +10,17 @@ export const ProductSchema = z.object({
       id: z.string(),
     }),
   ),
-  images: z.array(
-    z.object({
-      asset: z.object({
-        _ref: z.string(),
+  images: z
+    .array(
+      z.object({
+        asset: z.object({
+          _ref: z.string(),
+        }),
+        alt: z.string(),
       }),
-      alt: z.string(),
-    }),
-  ),
+    )
+    .min(1)
+    .nullable(),
   content: z.array(z.any()), // Portable text
   maxQuantityPerOrder: z.number().min(0).nullable(),
   pickupDates: z.array(z.iso.date()).nullable(),
